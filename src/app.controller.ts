0b0711ee31a3fc5415e4f2fed0a30eb5790d803c
xdong <hy44jt@gmail.com>
2023-07-31 13:46:01 +0900

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Inject, Req, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Request, Response } from 'express';
+import { SessionService } from './session/session.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Inject(SessionService)
+  private sessionService: SessionService;
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('count')
+  async count(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    const sid = req.cookies?.sid;
+
+    const session = await this.sessionService.getSession<{ count: string }>(
+      sid,
+    );
+
+    const curCount = session.count ? parseInt(session.count) + 1 : 1;
+    const curSid = await this.sessionService.setSession(sid, {
+      count: curCount,
+    });
+
+    res.cookie('sid', curSid, { maxAge: 1800000 });
+
+    return curCount;
+  }
+}
